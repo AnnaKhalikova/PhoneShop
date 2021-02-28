5bb5a73e0ee56f31edf8bb0a39fb1e041c6b7d4a
AnnaKhalikova <hanna.Khalikova@gmail.com>
2021-03-01 00:59:46 +0300

    Added new functionality

--- Phone.cs
+++ Phone.cs
@@ -14,18 +14,20 @@ namespace Phone_Shop
         public string Model { get; set; }
         public string Brand { get; set; }
         public bool IsAvailable { get; set; }
+        public Shop ShopPlace { get; set; }
         public OperatingSystemType Type { get; set; }
 
         public Phone()
         {
 
         }
-        public Phone(string model, string brand, OperatingSystemType type, bool isAvailable)
+        public Phone(string model, string brand, OperatingSystemType type, bool isAvailable, Shop shop)
         {
             Model = model;
             Brand = brand;
             Type = type;
             IsAvailable = isAvailable;
+            ShopPlace = shop; 
         }
     }
 }
--- Shop.cs
+++ Shop.cs
@@ -8,17 +8,21 @@ namespace Phone_Shop
     class Shop
     {
         List<Phone> Phones;
-        public Shop()
+        public string ShopName { get; set; }
+        public Shop(string shopName)
         {
+            ShopName = shopName;
             Phones = new List<Phone>();
         }
         void AddPhone(string model, string brand, OperatingSystemType type, bool isAvailable)
         {
             Phones.Add(new Phone(model, brand, type, isAvailable));
         }
-        Phone FindPhone(string model)
+        //remade search method with Dictionary structure
+        Phone FindPhone(string model, Shop shop)
         {
             Phone phone = null;
+            Dictionary<Phone, Shop> searchDictionary = new Dictionary<Phone, Shop>();
             foreach(var item in Phones)
             {
                 if(item.Model == model)
@@ -27,6 +31,23 @@ namespace Phone_Shop
                 }
             }
             return phone;
+        }
+        string ShowInfo(Phone phone)
+        {
+            string result = null;
+            if(phone == null)
+            {
+                result = "Введенный Вами товар не найден";
+            }
+            else if(phone != null == false)
+            {
+                result = "Данный товар отсутствует на складе";
+            }
+            else
+            {
+                result += "";
+            }
+            return result;
         }
     }
 }
